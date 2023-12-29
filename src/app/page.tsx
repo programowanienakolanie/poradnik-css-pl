c32287aa5a11ae4df88c43b98b6ecbec5872ecaa
RezixDev <mateuszrysownik2@gmail.com>
2023-12-29 08:42:39 +0100

    fix: remove unused import

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,6 +1,5 @@
 import { Header } from './components/Header/Header';
 import { Hero } from './components/Hero/Hero';
-import { CardList } from './components/CardList/CardList';
 
 import { CarouselList } from './components/CarouselList/CarouselList';
 import MaxWidthWrapper from './components/MaxWidthWrapper/MaxWidthWrapper';
